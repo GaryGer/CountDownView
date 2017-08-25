89a068d919c6afab65fdb167e0c1d1578671cbd7
GePengFei <18509297726@126.com>
2017-08-25 09:27:19 +0800

    Initial Commit

---
+++ CountdownModule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CountdownModule
+//
+//  Created by Ger on 2017/8/25.
+//  Copyright © 2017年 UEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
